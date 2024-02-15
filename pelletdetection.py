48c2ca625891256cfc97b5c5b9f82bc3dde4c3c0
597205 <597205@stud.hvl.no>
2024-02-15 15:09:50 +0100

    lagt til kamera

--- pelletdetection.py
+++ pelletdetection.py
@@ -3,8 +3,8 @@ from jetson_inference import detectNet
 from jetson_utils import videoSource, videoOutput
 
 net = detectNet("ssd-mobilenet-v2", threshold=0.5)
-camera = videoSource("csi://0")      # '/dev/video0' for V4L2
-display = videoOutput("display://0") # 'my_video.mp4' for file
+camera = videoSource("/dev/video0")      # '/dev/video0' for V4L2
+display = videoOutput() # 'my_video.mp4' for file
 
 while display.IsStreaming():
     img = camera.Capture()
