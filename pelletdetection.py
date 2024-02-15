9b5cf8c2acf4a17c26593a3b9008303a0705fa4c
597205 <597205@stud.hvl.no>
2024-02-15 15:04:59 +0100

    fjernet pls connection

--- pelletdetection.py
+++ pelletdetection.py
@@ -1,23 +1,6 @@
 #https://github.com/BO24EH-02/scripts.git
 from jetson_inference import detectNet
 from jetson_utils import videoSource, videoOutput
-from home import snap7
-
-IP = '192.168.0.1'  #PLC IP adress
-
-print(IP) 
-
-RACK = 0 
-
-SLOT = 0 
-
-PLC = snap7.client.Client() 
-
-if PLC.connect(IP,RACK,SLOT): #establish connections with Siemens PLC if ip is correct
-    print(PLC.get_cpu_state())
-else:
-    print("ERROR: PLC not connectet")    
-
 
 net = detectNet("ssd-mobilenet-v2", threshold=0.5)
 camera = videoSource("csi://0")      # '/dev/video0' for V4L2
