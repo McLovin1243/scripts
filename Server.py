6d1f7aa1913965d6822b63b5493d6542f30ad5ea
BO24EH-02 <bo24eh.02@gmail.com>
2024-03-06 09:15:26 +0100

    Fjernet duplikat

--- Server.py
+++ Server.py
@@ -2,12 +2,12 @@ import socket
 import threading
 #serverIP = socket.gethostbyname(socket.gethostname())
 serverIP = "192.168.0.3"
-Port = 5050 #PORT nvidia jetson
+Port = 5151 #PORT nvidia jetson
 ADDR = (serverIP,Port)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
-serverIP = socket.gethostbyname(socket.gethostname())
+
 
 
 def handle_client(conn, addr):
