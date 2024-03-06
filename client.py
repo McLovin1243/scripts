3c1d37aaf41870822aefc1c623a80ba0611c6be2
BO24EH-02 <bo24eh.02@gmail.com>
2024-03-06 15:41:26 +0100

    oppdatert client script

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import socket
 import time
+from jetson_inference import detectNet
+from jetson_utils import videoSource, videoOutput
 HEADER = 64
 port =5151
 FORMAT = 'utf-8'
@@ -16,6 +18,27 @@ def send(msg):
     client.send(send_length)
     client.send(message)
 
-while True:
-    send(input())
+
+
+
+net = detectNet("ssd-mobilenet-v2", threshold=0.5)
+camera = videoSource("/dev/video0")      # '/dev/video0' for V4L2
+display = videoOutput() # 'my_video.mp4' for file
+
+while display.IsStreaming():
+    img = camera.Capture()
+
+    if img is None: # capture timeout
+        continue
+
+    detections = net.Detect(img)
+    if detections:
+    	send('true')
+    else:
+    	send('false')
+    
+    
+    display.Render(img)
+    display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+
 
