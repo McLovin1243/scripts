7a058958e29598abaf8870bb674d0e9fefc547f9
597205 <597205@stud.hvl.no>
2024-04-16 13:42:06 +0200

    lagt til en forloop i detections

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import socket
 import time
 from jetson_inference import detectNet
 from jetson_utils import videoSource, videoOutput
+
 HEADER = 64
 port =5151
 FORMAT = 'utf-8'
@@ -19,11 +20,10 @@ def send(msg):
     client.send(message)
 
 
-
-
 net = detectNet(argv=['--model=models/rev10/ssd-mobilenet.onnx', '--labels=models/rev10/labels.txt', '--input-blob=input_0', '--output-cvg=scores', '--output-bbox=boxes', '--threshold=0.3'])
+
 print('********************* CHOOSE FILE OR WEBCAM *********************')
-print('enter f/w f is mp4 file and w is webcam')
+print('           enter f/w f is MP4 File and w is Webcam')
 a = input()
 if a == 'w' :
     camera = videoSource('/dev/video0')
@@ -32,19 +32,45 @@ if a == 'f':
     
 display = videoOutput() # 'my_video.mp4' for file
 
+
+
+
 while display.IsStreaming():
     img = camera.Capture()
 
     if img is None: # capture timeout
         continue
 
+   # detections = net.Detect(img)
+   # if detections:
+    #    print        
+    #	send('true')
+    #else:
+    #	send('false')
+
     detections = net.Detect(img)
-    if detections:
-    	send('true')
-    else:
-    	send('false')
+
+    for detections in detections:
+        class_name = net.GetClassDesc(detections.ClassID)
+        center = detections.Center
+        confidence = detections.Confidence
     
+        if class_name == "pellet" and confidence > 0.5:
+            pelletCounter =+ 1
+            print("PELLETS: "+pelletCounter)
+            send('false')
+        else:
+            send('true')
+
     
     display.Render(img)
     display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+
+
+
+
+
+
+
+
 
