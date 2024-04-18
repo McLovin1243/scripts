506aed7e0bb242bed582398e3b0b0fb6ec454311
bo24eh.02 <bo24eh.02@gmail.com>
2024-04-18 11:00:09 +0200

    lagt til vanlig deteksjon i tilegg

--- client.py
+++ client.py
@@ -20,51 +20,102 @@ def send(msg):
     client.send(message)
 
 
-net = detectNet(argv=['--model=models/rev10/ssd-mobilenet.onnx', '--labels=models/rev10/labels.txt', '--input-blob=input_0', '--output-cvg=scores', '--output-bbox=boxes', '--threshold=0.3'])
 
 print('********************* CHOOSE FILE OR WEBCAM *********************')
 print('           enter f/w f is MP4 File and w is Webcam')
 a = input()
+
+
+#Programkode for å kjøre eksisterende objektdetekterings sett.
 if a == 'w' :
+    net = detectNet("ssd-mobilenet-v2", threshold =0.5)
     camera = videoSource('/dev/video0')
-if a == 'f':
-    camera = videoSource('foring3.mp4')
-    
-display = videoOutput() # 'my_video.mp4' for file
+    display = videoOutput()
 
+    while display.IsStreaming():
+        img =camera.Capture()
 
+        if img is None:
+            continue
 
+        detections =net.Detect(img)
 
-while display.IsStreaming():
-    img = camera.Capture()
-
-    if img is None: # capture timeout
-        continue
-
-   # detections = net.Detect(img)
-   # if detections:
-    #    print        
-    #	send('true')
-    #else:
-    #	send('false')
-
-    detections = net.Detect(img)
-
-    for detections in detections:
-        class_name = net.GetClassDesc(detections.ClassID)
-        center = detections.Center
-        confidence = detections.Confidence
-    
-        if class_name == "pellet" and confidence > 0.5:
-            pelletCounter =+ 1
-            print("PELLETS: "+pelletCounter)
-            send('false')
+        if detections:
+            send("true")
         else:
-            send('true')
+            send("false")
+
+        display.Render(img)
+        display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+        
+
+
+#Programkode for å kjøre eget treningsett med pellet deteksjon
+
+if a == 'f':
+    net = detectNet(argv=['--model=models/rev11/ssd-mobilenet.onnx', '--labels=models/rev11/labels.txt', '--input-blob=input_0', '--output-cvg=scores', '--output-bbox=boxes', '--threshold=0.5'])
+    print("**************************Choose test file ***************************")
+    print("1. Klippet.MP4")
+    print("2. foring.MP4")
+    print("3. foring2.MP4")
+    print("4. foring3.MP4")
+    b = input()
+    source = "klippet.mp4"
+    
+    if(b == '1'):
+    	source = "klippet.mp4"
+    elif(b == '2'):
+    	source = "klippet.mp4"
+    elif(b == '3'):
+    	source = "klippet.mp4" 
+    elif(b == '4'):
+    	source = "testF.mp4"
+    else: 
+    	source = "klippet.mp4" 
+    camera = videoSource('foring3.mp4') 
+    display = videoOutput() # 'my_video.mp4' for file
+
+    while display.IsStreaming():
+        img = camera.Capture()
+        counter = 0
+        state = "true"
+
+        if img is None: # capture timeout
+            continue
+        detections = net.Detect(img)
+    
+        for detection in detections:
+            class_name = net.GetClassDesc(detection.ClassID)
+            center = detection.Center
+            confidence = detection.Confidence
+        
+    
+            if class_name == "pellet" and confidence > 0.5:
+                print("PELLETS:")
+                print(center)
+                counter +=1
+                if counter > 5:
+            	    state='false'   
+            elif counter < 5 and class_name == "salmon":
+                state='true'
 
     
-    display.Render(img)
-    display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+        print(counter)
+        send(state)
+  
+
+    
+        display.Render(img)
+        display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+    
+
+    
+        
+
+
+
+
+
 
 
 
