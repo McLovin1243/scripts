1a566977ab893961a0ec7f462cbfafd1ae8cfd11
Joakim <joagjerde@gmail.com>
2024-07-12 10:23:17 +0200

    jaaaa

--- popup.py
+++ popup.py
@@ -1,164 +1,126 @@
 import tkinter as tk
-from PIL import Image, ImageTk
+from threading import Thread
+from queue import Queue
 
-# Function to draw a rounded rectangle
-def draw_rounded_rectangle(canvas, x1, y1, x2, y2, radius, **kwargs):
-    points = [
-        x1 + radius, y1,
-        x2 - radius, y1,
-        x2, y1,
-        x2, y1 + radius,
-        x2, y2 - radius,
-        x2, y2,
-        x2 - radius, y2,
-        x1 + radius, y2,
-        x1, y2,
-        x1, y2 - radius,
-        x1, y1 + radius,
-        x1, y1,
-        x1 + radius, y1
-    ]
-    return canvas.create_polygon(points, smooth=True, **kwargs)
-def bigboat_rectangle(canvas, x1, y1, x2, y2, radius, **kwargs):
-    points = [
-        x1 + radius, y1,
-        x2 - radius, y1,
-        #x2, y1,
-        x2, y1 + radius,
-        x2, y2 - radius,
-        #x2, y2,
-        x2 - radius, y2,
-        x1 + radius, y2,
-        x1, y2,
-        x1, y2 - radius,
-        x1, y1 + radius,
-        x1, y1,
-        x1 + radius, y1
-    ]
-    return canvas.create_polygon(points, smooth=True, **kwargs)
+def server_gui(queue):
+    root = tk.Tk()
+    canvas = tk.Canvas(root, width=920, height=580, bg='light grey')
+    canvas.pack()
+        
 
-# Function to draw a rounded rectangle
-def draw_boat_rectangle(canvas, x1, y1, x2, y2, radius, **kwargs):
-    points = [
-        x1 + radius, y1,
-        x2 - radius, y1,
-        x2, y1,
-        x2, y1 + radius,
-        x2, y2 - radius,
-        #x2, y2,
-        x2 - radius, y2,
-        x1 + radius, y2,
-        #x1, y2,
-        x1, y2 - radius,
-        x1, y1 + radius,
-        x1, y1,
-        x1 + radius, y1
-    ]
-    return canvas.create_polygon(points, smooth=True, **kwargs)
+    def P1_ankomst():
+        queue.put("P1_gul")
 
-# Create the main window
-root = tk.Tk()
-root.title("Båtdeteksjon")
-
-# Create a canvas with full HD size (1920x1080)
-canvas = tk.Canvas(root, width=1920, height=1080, bg='light grey')
-canvas.pack()
-
-
-# Draw main components
-draw_rounded_rectangle(canvas, 510, 50, 1390, 165, 20, fill='gray12', outline='gray22', width=2)
-label = tk.Label(root, text="Haugesund Gjestekai Oversikt", bg='gray12', fg='azure', font=("Calibri", 52))
-canvas.create_window(530, 60, anchor='nw', window=label)
-hav =draw_rounded_rectangle(canvas, 99, 200, 1819, 980, 20, fill='DeepSkyBlue2', outline='gray22', width=25)
-
-image = Image.open("ocean.jpg")
-image = image.resize((1800 - 105, 960 - 202))
-photo = ImageTk.PhotoImage(image)
-
-# Create an image item on the canvas to cover 'hav'
-canvas.create_image(112, 212, anchor='nw', image=photo)
+    def P2_ankomst():
+        queue.put("P2_gul")
 
 
 
+    testbutton = tk.Button(canvas, text="Buttonaktivatefunction", bg='black', activebackground='red', bd=0, command=P2_ankomst)
+    canvas.create_window(250, 220, anchor='nw', window=testbutton, width=225, height=200)
 
-# Utstikkere
-utstikkere_pos = [(205, 595, 220, 750), (475, 595, 490, 750), (745, 595, 760, 750), (1015, 595, 1030, 750), (1285, 595, 1300, 750)]
-for pos in utstikkere_pos:
-    draw_rounded_rectangle(canvas, *pos, 10, fill='gray46', outline='gray46', width=0)
+    root.mainloop()
 
-# Brygge
-draw_rounded_rectangle(canvas, 190, 500, 1655, 600, 56, fill='gray76', outline='gray22', width=2)
-draw_rounded_rectangle(canvas, 1555, 500, 1655, 968, 56, fill='gray76', outline='gray22', width=2)
-draw_rounded_rectangle(canvas, 1554, 501, 1580, 599, 0, fill='gray76', outline='gray76', width=0)
+def secondary_gui(queue):
+    root = tk.Tk()
+    root.title("Haugesund Gjestebrygge")
 
-# Øvre store p-plasser
-"""
+    def draw_rounded_rectangle(canvas, x1, y1, x2, y2, radius, **kwargs):
+        points = [
+            x1 + radius, y1,
+            x2 - radius, y1,
+            x2, y1,
+            x2, y1 + radius,
+            x2, y2 - radius,
+            x2, y2,
+            x2 - radius, y2,
+            x1 + radius, y2,
+            x1, y2,
+            x1, y2 - radius,
+            x1, y1 + radius,
+            x1, y1,
+            x1 + radius, y1
+        ]
+        return canvas.create_polygon(points, smooth=True, **kwargs)
 
+    def P1_timer():
+        tidslabel.config(text='Parkert i 2 minutter')
 
-rect1 = bigboat_rectangle(canvas, 330, 330, 830, 480, 77, fill='white', outline='white', width=3) # P1!
-rect2 = bigboat_rectangle(canvas, 1000, 330, 1500, 480, 77, fill='white', outline='white', width=3)
+    def P2_timer():
+        tidslabel.config(text='Parkert i 5 minutter')
 
-canvas.create_oval(1440, 610, 1545, 830, fill='red', outline='red', width=2)
-canvas.create_oval(1440, 610, 1545, 830, fill='red', outline='red', width=2)
-
-
-
-rect3 = draw_boat_rectangle(canvas, 230, 610, 335, 850, 56, fill='snow2', outline='snow2', width=3)
-rect4 = draw_boat_rectangle(canvas, 360, 610, 465, 850, 56, fill='snow2', outline='snow2', width=3)
-rect5 = draw_boat_rectangle(canvas, 500, 610, 605, 850, 56, fill='gray90', outline='gray90', width=3)
-rect6 = draw_boat_rectangle(canvas, 630, 610, 735, 850, 56, fill='gray90', outline='gray90', width=3)
-rect7 = draw_boat_rectangle(canvas, 770, 610, 875, 850, 56, fill='white', outline='white', width=3)
-rect8 = draw_boat_rectangle(canvas, 900, 610, 1005, 850, 56, fill='white', outline='white', width=3)
-rect9 = draw_boat_rectangle(canvas, 1040, 610, 1145, 850, 56, fill='white', outline='white', width=3) # P2!
-rect10 = draw_boat_rectangle(canvas, 1170, 610, 1275, 850, 56, fill='white', outline='white', width=3)
-rect11 = draw_boat_rectangle(canvas, 1310, 610, 1415, 850, 56, fill='white', outline='white', width=3)
-#rect12 = draw_rounded_rectangle(canvas, 1440, 610, 1545, 830, 65, fill='white', outline='white', width=3)
-"""
-
-
-boat1= canvas.create_oval(280, 330, 780, 480, fill='', outline='', width=2) #P1
-boat2= canvas.create_oval(1020, 330, 1520, 480, fill='', outline='', width=2)
-
-boat3 = canvas.create_oval(230, 610, 335, 850, fill='', outline='', width=2)
-boat4 = canvas.create_oval(360, 610, 465, 850, fill='', outline='', width=2)
-boat5 = canvas.create_oval(500, 610, 605, 850, fill='', outline='', width=2)
-boat6 = canvas.create_oval(630, 610, 735, 850, fill='', outline='', width=2)
-boat7 = canvas.create_oval(770, 610, 875, 850, fill='', outline='', width=2)
-boat8 = canvas.create_oval(900, 610, 1005, 850, fill='', outline='', width=2)
-boat9 = canvas.create_oval(1040, 610, 1145, 850, fill='', outline='', width=2) #P2
-boat10 = canvas.create_oval(1170, 610, 1275, 850, fill='', outline='', width=2)
-boat11 = canvas.create_oval(1310, 610, 1415, 850, fill='', outline='', width=2)
-boat12 = canvas.create_oval(1440, 610, 1545, 850, fill='', outline='', width=2)
-
-# Create and place the buttons
-#button1 = tk.Button(canvas, text="", bg=None, activebackground=None, bd=0, highlightthickness=0, command=lambda: display_time("10:00"))
-#button1_window = canvas.create_window(340, 340, anchor='nw', window=button1, width=380, height=130)
-
-#button2 = tk.Button(canvas, text="", bg="white", activebackground="gray98", bd=0, command=lambda: display_time("11:30"))
-#button2_window = canvas.create_window(1010, 340, anchor='nw', window=button2, width=380, height=130)
-
-#button3 = tk.Button(canvas, text="", bg="white", activebackground="gray98", bd=0, command=lambda: display_time("13:15"))
-#button3_window = canvas.create_window(1050, 620, anchor='nw', window=button3, width=85, height=150)
+    def process_queue():
+        while not queue.empty():
+            message = queue.get()
+            if message == "P1_gul":
+                canvas.itemconfig(guiP1, fill='yellow', outline="yellow")
+                button1.config(bg='yellow', activebackground='yellow')
+            elif message == "P2_gul":
+                canvas.itemconfig(guiP2, fill='yellow', outline="yellow")
+                button2.config(bg='yellow', activebackground='yellow')    
 
 
+        root.after(100, process_queue)  # Check the queue periodically
 
-# Label for displaying time
-time_label = tk.Label(canvas, text="", font=("Calibri", 20), bg="light gray")
-time_label_window = canvas.create_window(120, 140, anchor='nw', window=time_label)
+    canvas = tk.Canvas(root, width=1920, height=1080, bg='light grey')
+    canvas.pack()
 
-# Function to display time amount
-def signal():
-    test1 = input()
-    if test1=="a":
-        canvas.itemconfig(boat9, fill='green2', outline='green2')
-        button9 = tk.Button(canvas, text="", bg="green2", activebackground='lime green', bd=0)
-        button9_window = canvas.create_window(1053, 656, anchor='nw', window=button9, width=81, height=150)
-    test2 = input()
-    if test2 == "a":
-        canvas.itemconfig(boat9, fill='red', outline='red')
-        button9.config(bg='red', activebackground='red2')
+    draw_rounded_rectangle(canvas, 619, 50, 1300, 155, 20, fill='gray12', outline='gray22', width=2)  # Title box
 
-signal()
+    label = tk.Label(root, text="Haugesund Gjestebrygge", bg='gray12', fg='azure', font=("Calibri", 46))
+    canvas.create_window(643, 60, anchor='nw', window=label)
 
-# Start the Tkinter main loop
-root.mainloop()
+    hav = draw_rounded_rectangle(canvas, 99, 200, 1819, 980, 20, fill='#9ACBFF', outline='gray22', width=25)
+
+    # Utstikkere
+    draw_rounded_rectangle(canvas, 205, 595, 220, 750, 10, fill='gray46', outline='gray12', width=0)
+    draw_rounded_rectangle(canvas, 475, 595, 490, 750, 10, fill='gray46', outline='gray12', width=0)
+    draw_rounded_rectangle(canvas, 745, 595, 760, 750, 10, fill='gray46', outline='gray12', width=0)
+    draw_rounded_rectangle(canvas, 1015, 595, 1030, 750, 10, fill='gray46', outline='gray12', width=0)
+    draw_rounded_rectangle(canvas, 1285, 595, 1300, 750, 10, fill='gray46', outline='gray12', width=0)
+
+    # Draw a rounded rectangle for 'brygge'
+    draw_rounded_rectangle(canvas, 200, 500, 1655, 600, 15, fill='gray76', outline='gray22', width=2)
+    draw_rounded_rectangle(canvas, 1555, 500, 1655, 968, 15, fill='gray76', outline='gray22', width=2)
+    draw_rounded_rectangle(canvas, 1554, 501, 1560, 599, 0, fill='gray76', outline='gray76', width=0)
+
+    # Rektangler for øvre store p-plasser
+    guiP1 = draw_rounded_rectangle(canvas, 330, 330, 830, 480, 60, fill='#D4EAFF', outline='#D4EAFF', width=3)
+    draw_rounded_rectangle(canvas, 1000, 330, 1500, 480, 60, fill='#D4EAFF', outline='#D4EAFF', width=3)
+
+    # Nedre P-plasser
+    draw_rounded_rectangle(canvas, 230, 610, 335, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=0)
+    draw_rounded_rectangle(canvas, 360, 610, 465, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=0)
+    draw_rounded_rectangle(canvas, 500, 610, 605, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=0)
+    draw_rounded_rectangle(canvas, 630, 610, 735, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=0)
+    draw_rounded_rectangle(canvas, 770, 610, 875, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=0)
+    draw_rounded_rectangle(canvas, 900, 610, 1005, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=0)
+    guiP2 = draw_rounded_rectangle(canvas, 1040, 610, 1145, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=0)
+    draw_rounded_rectangle(canvas, 1170, 610, 1275, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=0)
+    draw_rounded_rectangle(canvas, 1310, 610, 1415, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=0)
+    draw_rounded_rectangle(canvas, 1440, 610, 1545, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=0)
+
+    # Create and place the buttons
+    button1 = tk.Button(canvas, text="", bg='#D4EAFF', activebackground='#D0E5FA', bd=0, command=lambda: P1_timer())
+    canvas.create_window(340, 340, anchor='nw', window=button1, width=480, height=130)
+    button2 = tk.Button(canvas, text="", bg='#D4EAFF', activebackground='#D0E5FA', bd=0, command=lambda: P2_timer())
+    canvas.create_window(1050, 620, anchor='nw', window=button2, width=85, height=200)
+
+    tidslabel = tk.Label(canvas, text="", bg='light gray', fg='gray12', font=('Calibri', 32))
+    canvas.create_window(110, 188, anchor='sw', window=tidslabel)
+
+    root.after(100, process_queue)  # Start checking the queue
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    queue = Queue()
+
+    server_thread = Thread(target=server_gui, args=(queue,))
+    secondary_thread = Thread(target=secondary_gui, args=(queue,))
+
+    server_thread.start()
+    secondary_thread.start()
+
+    server_thread.join()
+    secondary_thread.join()
--- serverGUI_copy.py
+++ serverGUI_copy.py
@@ -379,8 +379,6 @@ class App:
 
     # FUNCTION OPEN SURVEILLANCE
     def btnOpen(self):
-        bredde = 0
-
         # Function to draw a rounded rectangle
         def draw_rounded_rectangle(canvas, x1, y1, x2, y2, radius, **kwargs):
             points = [
@@ -398,25 +396,53 @@ class App:
                 x1, y1,
                 x1 + radius, y1
             ]
-            canvas.create_polygon(points, smooth=True, **kwargs)
+            return canvas.create_polygon(points, smooth=True, **kwargs)
+
+
+        def P1_timer(): ### KAN VÆRE VI MÅ HA GLOBAL
+            tidslabel.config(text='Parkert i 2 minutter')
+
+        def P2_timer():
+            tidslabel.config(text='Parkert i 5 minutter')
+
+        def P1_ankomst():
+            canvas.itemconfig(guiP1, fill='yellow', outline="yellow")
+            button1.config(bg='yellow', activebackground='yellow')
+        def P2_ankomst():
+            canvas.itemconfig(guiP2, fill='yellow',outline="yellow")
+            button2.config(bg='yellow', activebackground='yellow')
+
+        def betalt():
+            canvas.itemconfig(guiP1, fill='#2EC729', outline="#2EC729")
+            button1.config(bg='#2EC729', activebackground='#2EC729')
+            canvas.itemconfig(guiP2, fill='#2EC729',outline="#2EC729")
+            button2.config(bg='#2EC729', activebackground='#2EC729')
+
+        def P1_alarm():
+            canvas.itemconfig(guiP1,fill='red',outline='red')
+            button1.config(bg='red',activebackground='red')
+
+        def P2_alarm():
+            canvas.itemconfig(guiP2,fill='red',outline='red')
+            button2.config(bg='red',activebackground='red')
+
+
 
         # Create the main window
         root = tk.Tk()
-        root.title("Boatdeteksjon")
+        root.title("Haugesund Gjestebrygge")
 
 
         # Create a canvas with full HD size (1920x1080)
         canvas = tk.Canvas(root, width=1920, height=1080, bg='light grey')
         canvas.pack()
 
-
         draw_rounded_rectangle(canvas, 619, 50, 1300, 155, 20, fill='gray12', outline='gray22', width=2) # Title box
 
         label = tk.Label(root, text="Haugesund Gjestebrygge", bg='gray12', fg='azure', font=("Calibri", 46))
         canvas.create_window(643, 60, anchor='nw', window=label)
 
-        # Draw a rounded rectangle for 'hav'
-        draw_rounded_rectangle(canvas, 99, 200, 1819, 980, 20, fill='#9ACBFF', outline='gray22', width=25)
+        hav = draw_rounded_rectangle(canvas, 99, 200, 1819, 980, 20, fill='#9ACBFF', outline='gray22', width=25)
 
 
 
@@ -433,31 +459,31 @@ class App:
         draw_rounded_rectangle(canvas, 1554, 501, 1560, 599, 0, fill='gray76', outline='gray76', width=0)
 
         # Rektangler for øvre store p-plasser
-        draw_rounded_rectangle(canvas, 330, 330, 830, 480, 60, fill='#D4EAFF', outline='#D4EAFF', width=3)
+        guiP1 = draw_rounded_rectangle(canvas, 330, 330, 830, 480, 60, fill='#D4EAFF', outline='#D4EAFF', width=3)
         draw_rounded_rectangle(canvas, 1000, 330, 1500, 480, 60, fill='#D4EAFF', outline='#D4EAFF', width=3)
 
         # Nedre P-plasser
-        draw_rounded_rectangle(canvas, 230, 610, 335, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=bredde)
-        draw_rounded_rectangle(canvas, 360, 610, 465, 830, 65, fill='#2EC729', outline='#2EC729', width=bredde)
-        draw_rounded_rectangle(canvas, 500, 610, 605, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=bredde)
-        draw_rounded_rectangle(canvas, 630, 610, 735, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=bredde)
-        draw_rounded_rectangle(canvas, 770, 610, 875, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=bredde)
-        draw_rounded_rectangle(canvas, 900, 610, 1005, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=bredde)
-        draw_rounded_rectangle(canvas, 1040, 610, 1145, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=bredde)
-        draw_rounded_rectangle(canvas, 1170, 610, 1275, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=bredde)
-        draw_rounded_rectangle(canvas, 1310, 610, 1415, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=bredde)
-        draw_rounded_rectangle(canvas, 1440, 610, 1545, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=bredde)
+        draw_rounded_rectangle(canvas, 230, 610, 335, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=0)
+        draw_rounded_rectangle(canvas, 360, 610, 465, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=0)
+        draw_rounded_rectangle(canvas, 500, 610, 605, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=0)
+        draw_rounded_rectangle(canvas, 630, 610, 735, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=0)
+        draw_rounded_rectangle(canvas, 770, 610, 875, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=0)
+        draw_rounded_rectangle(canvas, 900, 610, 1005, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=0)
+        guiP2 = draw_rounded_rectangle(canvas, 1040, 610, 1145, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=0)
+        draw_rounded_rectangle(canvas, 1170, 610, 1275, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=0)
+        draw_rounded_rectangle(canvas, 1310, 610, 1415, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=0)
+        draw_rounded_rectangle(canvas, 1440, 610, 1545, 830, 65, fill='#D4EAFF', outline='#D4EAFF', width=0)
 
         # Create and place the buttons
-        button1 = tk.Button(canvas, text="", bg='#D4EAFF', activebackground='#D0E5FA', bd=0, command=lambda: function("input"))
+        button1 = tk.Button(canvas, text="", bg='#D4EAFF', activebackground='#D0E5FA', bd=0, command=lambda: P1_timer())
         button1_window = canvas.create_window(340, 340, anchor='nw', window=button1, width=480, height=130)
-        button2 = tk.Button(canvas, text="", bg='#D4EAFF', activebackground='#D0E5FA', bd=0, command=lambda: function("input"))
-        button2_window = canvas.create_window(1010, 340, anchor='nw', window=button2, width=480, height=130)
-        button3 = tk.Button(canvas, text="", bg='#D4EAFF', activebackground='#D0E5FA', bd=0, command=lambda: function("input"))
-        button3_window = canvas.create_window(1050, 620, anchor='nw', window=button3, width=85, height=200)
+        button2 = tk.Button(canvas, text="", bg='#D4EAFF', activebackground='#D0E5FA', bd=0, command=lambda: P2_timer())
+        button2_window = canvas.create_window(1050, 620, anchor='nw', window=button2, width=85, height=200)
 
-        def function(verdi1):
-             print(verdi1)
+        tidslabel = tk.Label(canvas, text="", bg='light gray',fg='gray12', font=('Calibri',32))
+        canvas.create_window(110,188, anchor='sw', window=tidslabel)
+
+
 
 if __name__ == "__main__":
     root = tk.Tk()
